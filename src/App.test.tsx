0f70eba2b5d9b69a57fd5faac1b1b118d25efa18
KimberlyPocco <kimberly.poccop@gmail.com>
2023-01-26 21:08:37 -0500

    changed src app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import './App.css';
-import Pokedex from './components/Pokedex';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
-function App() {
-  return (
-    <Pokedex/>
-  );
-}
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
